b9f6c96540eda019166ba3df0ac5255515fa2291
Arnaud HIPPOLYTE <ahippoly@e1r10p18.42.fr>
2019-09-25 02:45:45 +0200

    This is a commit

---
+++ ArnaudEVOLVED/main2.c
@@ -0,0 +1,307 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahippoly <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/16 23:51:11 by ahippoly          #+#    #+#             */
+/*   Updated: 2019/09/25 00:52:41 by ahippoly         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf.h"
+
+void draw_diag(int size, SDL_Texture *text)
+{
+	char *pixel_tab;
+	int i;
+
+	pixel_tab = (char*)malloc(size * size * 4);
+	i = 0;
+	while (i < size * 4)
+	{
+		pixel_tab[i * size  + i] = (char)255;
+		pixel_tab[i * size  + i + 3] = (char)255;
+		i += 4;
+	}
+	SDL_UpdateTexture(text,NULL,pixel_tab,size*4);
+	free(pixel_tab);
+}
+
+void set_rect(SDL_Rect *size, int w, int h)
+{
+	size->x = 0;
+	size->y = 0;
+	size->w = w;
+	size->h = h;
+}
+
+void ptab_ini(char *p_tab, int floor_h)
+{
+	int x;
+	int y;
+
+	x = 0;
+	y = 0;
+
+	while (y < WIN_SIZE / floor_h)
+	{
+		x = 0;
+		while (x < WIN_SIZE)
+		{
+			p_tab[(y * WIN_SIZE + x) * 4] = (char)255;
+			p_tab[(y * WIN_SIZE + x) * 4 + 3] = (char)255;
+			x++;
+		}
+		y++;
+	}
+	while (y < WIN_SIZE)
+	{
+		x = 0;
+		while (x < WIN_SIZE)
+		{
+			p_tab[(y * WIN_SIZE + x) * 4] = 0;
+			p_tab[(y * WIN_SIZE + x) * 4 + 3] = (char)255;
+			x++;
+		}
+		y++;
+	}
+}
+
+void put_rect(char *pixel, SDL_Point pos, SDL_Point size, int color, SDL_Point win_size)
+{
+	int i;
+	int j;
+	SDL_Point end;
+	unsigned int *tab;
+	int negx;
+	int negy;
+
+	tab = (unsigned int*)pixel;
+	negx = 1;
+	negy = 1;
+	if (size.y < 0)
+	{
+		size.y = -size.y;
+		negy = -1;
+	}
+	if (size.x < 0)
+	{
+		size.x = -size.x;
+		negx = -1;
+	}
+	end.x = pos.x + size.x;
+	end.y = pos.y + size.y;
+	if (end.x > win_size.x)
+		end.x = win_size.x;
+	if (end.y > win_size.y)
+		end.y = win_size.y;
+	if (pos.x < 0)
+		pos.x = 0;
+	if (pos.y < 0)
+		pos.y = 0;
+	i = pos.y;
+	while (i < end.y)
+	{
+		j = pos.x;
+		while (j < end.x)
+		{
+			tab[i * win_size.x + j] = color;
+			j++;
+		}
+		i++;
+	}
+}
+
+void put_view_ray(char *pixel, SDL_Point cube_pos[2], SDL_Point pos, float rot, char color)
+{
+	SDL_Point endpoint;
+
+	endpoint.x = pos.x - (100 * cos(rot * M_PI_2));
+	endpoint.y = pos.y - (100 * sin(rot * M_PI_2));
+	octant(pos, endpoint, pixel, color);
+}
+/*
+void clear_tab_ini(t_var *v)
+{
+	unsigned int *tab = (unsigned int*)v->p_clear;
+	int i;
+	int end;
+
+	i = 0;
+	end = WIN_SIZE * WIN_SIZE;
+	while (i < end)
+		tab[i++] = 0x000000ff;
+}
+*/
+void var_ini(t_var *v, char *file)
+{
+	v->win = SDL_CreateWindow("test",
+						   SDL_WINDOWPOS_CENTERED,
+						   SDL_WINDOWPOS_CENTERED,
+						   WIN_SIZE,
+						   WIN_SIZE,
+						   SDL_WINDOW_SHOWN);
+	v->rend = SDL_CreateRenderer(v->win, -1, SDL_RENDERER_ACCELERATED);
+	v->screen = SDL_CreateTexture(v->rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, WIN_SIZE, WIN_SIZE);
+	v->mini_map_xy = SDL_CreateTexture(v->rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, MINI_MAP_W, MINI_MAP_H);
+	v->mini_map_xz = SDL_CreateTexture(v->rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, MINI_MAP_W, MINI_MAP_H);
+	v->mini_map_yz = SDL_CreateTexture(v->rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, MINI_MAP_W, MINI_MAP_H);
+	v->character = SDL_CreateTexture(v->rend, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STREAMING, 10, 10);
+	v->p_tab = (char*)malloc(WIN_SIZE * WIN_SIZE * 4);	
+	v->map = readfile2(file, v);
+	printf("map_size : x = %f, y = %f, z = %f\n",v->map_size.x ,v->map_size.y, v->map_size.z);
+	v->perso_pos.x = 1;
+	v->perso_pos.y = 1;
+	v->perso_pos.z = -1.5;
+	v->fov = 1;
+	v->rot = 1;
+	v->roty = 1;
+	v->win_size.x = WIN_SIZE;
+    v->win_size.y = WIN_SIZE;
+	v->stone.pixels = ft_load_bmp("img/stones.bmp",&v->stone.w, &v->stone.h);
+	printf("IMG_SIZE, x = %i, y = %i\n",v->img_size.x, v->img_size.y);
+	//clear_tab_ini(v);
+	SDL_SetTextureBlendMode(v->mini_map_xy, SDL_BLENDMODE_ADD);
+	SDL_SetTextureBlendMode(v->mini_map_xz, SDL_BLENDMODE_ADD);
+	SDL_SetTextureBlendMode(v->mini_map_yz, SDL_BLENDMODE_ADD);
+	SDL_SetRenderDrawBlendMode(v->rend, SDL_BLENDMODE_BLEND);
+//	SDL_SetTextureBlendMode()
+	printf("test\n");
+}
+
+int main(int ac, char **av)
+{
+	SDL_Event e;
+	SDL_Window *win;
+	SDL_Texture *screen;
+	SDL_Texture *view;
+	SDL_Rect image_size;
+	void *pixel_view;
+	int pitch;
+	int w;
+	int h;
+	int quit = 0;
+	float posx;
+	float posy;
+	int **map;
+	int map_h;
+	int map_w;
+	int floor_h;
+	float coef_x;
+	float coef_y;
+	SDL_Point perso;
+	SDL_Point perso_size;
+	SDL_Point map_size;
+	float rot;
+	t_var v;
+
+	ac = 1;
+	SDL_Init(SDL_INIT_VIDEO);
+	var_ini(&v,av[1]);
+	printf("segfault 1?\n");
+	printf("segfault2\n");
+	printf("segfault2 33 \n");
+	map_size.x = 100;
+	map_size.y = 100;
+	map_ini(v.map, map_size, &v);
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = 1;
+			}
+			if (e.type == SDL_KEYDOWN)
+			{
+				if (e.key.keysym.scancode == SDL_SCANCODE_W)
+				{
+					v.perso_pos.y += 0.1 * cos((v.rot) * M_PI_2);
+					v.perso_pos.x += 0.1 * sin((v.rot) * M_PI_2);
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_S)
+				{
+					v.perso_pos.y -= 0.1 * cos((v.rot) * M_PI_2);
+					v.perso_pos.x -= 0.1 * sin((v.rot) * M_PI_2);
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_A)
+				{
+					v.perso_pos.y += 0.1 * cos((v.rot + 1) * M_PI_2);
+					v.perso_pos.x += 0.1 * sin((v.rot + 1) * M_PI_2);
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_D)
+				{					
+					v.perso_pos.y -= 0.1 * cos((v.rot + 1) * M_PI_2);
+					v.perso_pos.x -= 0.1 * sin((v.rot + 1) * M_PI_2);
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_Q)
+				{
+					v.rot += (double)0.02;
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_E)
+				{
+					v.rot -= (double)0.02;
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_F)
+				{
+					v.perso_pos.z += (double)0.1;
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_R)
+				{
+					v.perso_pos.z -= (double)0.1;
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_T)
+				{
+					v.roty += (double)0.02;
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_G)
+				{
+					v.roty -= (double)0.02;
+					print_map(&v);
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_Z)
+				{
+					//	if (v.fov < 10)
+					{
+						v.fov += (double)0.05;
+						print_map(&v);
+					}
+				}
+				if (e.key.keysym.scancode == SDL_SCANCODE_X)
+				{
+					if (v.fov > 0.051)
+					{
+						v.fov -= (double)0.05;
+						print_map(&v);
+					}
+				}
+				printf("       rotx = %f \n",v.rot);
+				printf("       roty = %f \n",v.roty);
+				printf("perso_pos.x = %f \n",v.perso_pos.x);
+				printf("perso_pos.y = %f \n",v.perso_pos.y);
+				printf("perso_pos.z = %f \n",v.perso_pos.z);
+				printf("        fov = %f \n",v.fov);
+				printf("key pressed = %i \n",e.key.keysym.scancode);
+			}
+			if (e.type == SDL_MOUSEBUTTONDOWN)
+			{
+				quit = 1;
+			}
+		}
+	}
+
+//	SDL_FreeFormat(format);
+	//SDL_Quit();
+	return (0);
+}
---
+++ ArnaudEVOLVED/octant2.c
@@ -0,0 +1,191 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_display.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahippoly <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/31 14:02:22 by ahippoly          #+#    #+#             */
+/*   Updated: 2019/09/25 01:04:12 by ahippoly         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "wolf.h"
+
+int ft_abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	return (a);
+}
+
+void	oct_ini(t_oct *oct, SDL_Point pos1, SDL_Point pos2, int pos[2][2])
+{
+	pos[0][0] = pos1.x;
+	pos[0][1] = pos2.x;
+	pos[1][0] = pos1.y;
+	pos[1][1] = pos2.y;
+	oct->inc[0] = pos2.x - pos1.x > 0 ? 1 : -1;
+	oct->inc[1] = pos2.y - pos1.y > 0 ? 1 : -1;
+	oct->d[0] = (pos2.x - pos1.x) * 2 * oct->inc[0];
+	oct->d[1] = (pos2.y - pos1.y) * 2 * oct->inc[1];
+	oct->bool = oct->d[0] > oct->d[1] ? 1 : 0;
+	oct->boolxy = 1 - oct->bool;
+	oct->e = oct->d[oct->boolxy] / 2;
+	oct->ecart = (pos[oct->boolxy][1] - pos[oct->boolxy][0])
+		* oct->inc[oct->boolxy];
+}
+
+void	octant(SDL_Point pos1, SDL_Point pos2, char *pixel, int color)
+{
+	t_oct			oct;
+	int				pos[2][2];
+	float			i;
+	unsigned int	*p_tab;
+
+	if (pos1.x != -10000 && pos2.x != -10000)
+	{
+		p_tab = (unsigned int*)pixel;
+		i = 0;
+		oct_ini(&oct, pos1, pos2, pos);
+		while (pos[oct.boolxy][0] != pos[oct.boolxy][1])
+		{
+			if (pos[0][0] < WIN_SIZE && pos[0][0] > 0 && pos[1][0] > 0
+				&& pos[1][0] < WIN_SIZE)
+				p_tab[pos[0][0] + pos[1][0] * WIN_SIZE] = color;
+			if ((oct.e -= oct.d[oct.bool]) <= 0)
+			{
+				pos[oct.bool][0] += oct.inc[oct.bool];
+				oct.e += oct.d[oct.boolxy];
+			}
+			i++;
+			pos[oct.boolxy][0] += oct.inc[oct.boolxy];
+		}
+	}
+}
+
+SDL_Point	*mem_octant(SDL_Point pos1, SDL_Point pos2, int *length)
+{
+	t_oct			oct;
+	int				pos[2][2];
+	int				i;
+	SDL_Point		*pos_tab;
+
+	*length = 0;
+	pos_tab = NULL;
+	if (pos1.x != -10000 && pos2.x != -10000)
+	{
+		i = 0;
+		oct_ini(&oct, pos1, pos2, pos);
+		*length = ft_abs(pos[oct.boolxy][0] - pos[oct.boolxy][1]);
+		pos_tab = (SDL_Point*)malloc(sizeof(SDL_Point) * *length);
+		while (pos[oct.boolxy][0] != pos[oct.boolxy][1])
+		{
+			pos_tab[i].x = pos[0][0];
+			pos_tab[i].y = pos[1][0];
+			if ((oct.e -= oct.d[oct.bool]) <= 0)
+			{
+				pos[oct.bool][0] += oct.inc[oct.bool];
+				oct.e += oct.d[oct.boolxy];
+			}
+			i++;
+			pos[oct.boolxy][0] += oct.inc[oct.boolxy];
+		}
+	}
+	return (pos_tab);
+}
+
+void	fill_rectangle(t_line top, t_line bot, char *pixel, int color)
+{
+	t_oct			oct[2];
+	int				pos[2][2][2];
+	double 			dist[2];
+	unsigned int	*p_tab;
+	int 			longer;
+
+	p_tab = (unsigned int*)pixel;
+	oct_ini(&oct[0], top.pos1, top.pos2, pos[0]);
+	oct_ini(&oct[1], bot.pos1, bot.pos2, pos[1]);
+	dist[0] = hypot(top.pos2.x - top.pos1.x, top.pos2.y - top.pos1.y);
+	dist[1] = hypot(bot.pos2.x - bot.pos1.x, bot.pos2.y - bot.pos1.y);
+	longer = dist[1] > dist[0] ? 1 : 0;
+
+	while (pos[longer][oct[longer].boolxy][0] != pos[longer][oct[longer].boolxy][1])
+	{
+		if (pos[longer][0][0] < WIN_SIZE && pos[longer][0][0] > 0 && pos[longer][1][0] > 0
+			&& pos[longer][1][0] < WIN_SIZE)
+			p_tab[pos[longer][0][0] + pos[longer][1][0] * WIN_SIZE] = color;
+		if ((oct[longer].e -= oct[longer].d[oct[longer].bool]) <= 0)
+		{
+			pos[longer][oct[longer].bool][0] += oct[longer].inc[oct[longer].bool];
+			oct[longer].e += oct[longer].d[oct[longer].boolxy];
+		}
+		pos[longer][oct[longer].boolxy][0] += oct[longer].inc[oct[longer].boolxy];
+	}
+}
+
+void disp_text(char *pixels, char *img_tab, SDL_Point size)
+{
+	unsigned int *screen;
+	unsigned int *img;
+	int i;
+	int j;
+
+
+	screen = (unsigned int*)pixels;
+	img = (unsigned int*)img_tab;
+	j = 0;
+	while (j < size.y)
+	{
+		i = 0;
+		while (i < size.x)
+		{
+			screen[i + j * WIN_SIZE] = img[i + j * size.x];
+			i++;
+		}
+		j++;
+	}
+}
+
+void    bresenham_texture(t_line line, char *pixels, t_text *text, double x_ratio)
+{
+	t_oct           oct;
+    int             pos[2][2];
+    float           i;
+	float			step;
+    unsigned int    *p_tab;
+	unsigned int	*text_pix;
+	int				text_x_ratio;
+
+    if (line.pos1.x != -10000 && line.pos2.x != -10000)
+    {
+		p_tab = (unsigned int*)pixels;
+		text_pix = (unsigned int*)text->pixels;
+        i = 0;
+        oct_ini(&oct, line.pos1, line.pos2, pos);
+		step = (double)text->w / ft_abs(pos[oct.boolxy][1] - pos[oct.boolxy][0]);
+		text_x_ratio = (int)(x_ratio * text->w);
+//		printf("x_ratio = %i\n",text_x_ratio);
+        while (pos[oct.boolxy][0] != pos[oct.boolxy][1])
+        {
+            if (pos[0][0] < WIN_SIZE && pos[0][0] > 0 && pos[1][0] > 0
+                && pos[1][0] < WIN_SIZE)
+			{
+                p_tab[pos[0][0] + pos[1][0] * WIN_SIZE] = text_pix[text_x_ratio + ((int)i * text->w)];
+//                p_tab[pos[0][0] + pos[1][0] * WIN_SIZE] = text_pix[512*512 - 1];
+//                p_tab[pos[0][0] + pos[1][0] * WIN_SIZE] = text_pix[(int)(size.x * x_ratio)];
+//                p_tab[pos[0][0] + pos[1][0] * WIN_SIZE] = 0xff000000 | (int)(x_ratio * 256);
+//                p_tab[pos[0][0] + pos[1][0] * WIN_SIZE] = 0xff000000 | ((int)(i * 65536) & 0xff00ff00) | (int)(x_ratio * 256);
+//				printf("current y ratio = %i\n",(int)(i));
+			}
+			if ((oct.e -= oct.d[oct.bool]) <= 0)
+            {
+                pos[oct.bool][0] += oct.inc[oct.bool];
+                oct.e += oct.d[oct.boolxy];
+            }
+            i += step;
+            pos[oct.boolxy][0] += oct.inc[oct.boolxy];
+        }
+    }
+//	printf("i = %f \n", i);
+}
